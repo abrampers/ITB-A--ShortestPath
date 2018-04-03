f8e2d83c71a388425f6559872b178d0099ae75fa
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-03 20:51:19 +0700

    Changed the implementation of the matrix and some cleaning up

--- astar.py
+++ astar.py
@@ -1,33 +1,39 @@
-import heapq 
-import math
+'''
+Note that with this design, 
+* Dead ends will be evicted permanently from the queue
+* If the end node can't be reached, it will infinitely loop
+'''
 
-# The map is represented by a list of the node cordinates and a boolean matrix that tells whether 2 nodes are connected 
-nodes = [(1,2), (3,4), (5,6)] # Node coordinates, to compute the euclidean distance
-weights = [] # Contains edge weights. Disconnected nodes = -1. Row = from
-weights.append([-1, 1, -1])
-weights.append([-1, -1, 2])
-weights.append([-1. -1, -1])
+from heapq import heappush, heappop
+from math import sqrt
+
+# The map is represented by a tuple of nodes coordinate and a boolean matrix that tells whether 2 nodes are connected 
+nodes = ((0,0), (1,0), (1,1), (1.5,1.5), (2,2)) 
+is_connected = [] # Row = from
+is_connected.append([False, True, True, False, False])
+is_connected.append([True, False, False, False, False])
+is_connected.append([True, False, False, True, True])
+is_connected.append([False, False, True, False, True])
+is_connected.append([False, False, True, True, False])
 start_node = 0 
-end_node = 2 
+end_node = 4 
 q = [] # Prio-Q for A*. (distance, (node1, node2, ...))
 
-def eDistance(node1, node2):
-	return math.sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
+def eDistance(node1, node2): return sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
 
-# Initialize heapq with the start node
-for node in range(0, len(nodes)) :
-	if weights[start_node][node] != -1:
-		heapq.heappush(q, (weights[start_node][node] + eDistance(nodes[node], nodes[end_node]), (start_node, node)))
+# Initialize q with the start node
+for dest_node in range(0, len(nodes)) :
+	if is_connected[start_node][dest_node]:
+		heappush(q, (eDistance(nodes[start_node], nodes[dest_node]) + eDistance(nodes[dest_node], nodes[end_node]), (start_node, dest_node)))
 # Start the A* algorithm
-while True:
-	if int(q[0][1][-1]) == end_node: break # If the shortest path yet is able to reach the end node, break
-	prev_nodes = heapq.heappop(q)[1]
+while q[0][1][-1] != end_node: # If the shortest path yet is able to reach the end node, break
+	prev_nodes = heappop(q)[1]
 	# Compute previous distance
 	prev_weight = 0
-	for x in prev_nodes[1:]: prev_weight += weights[prev_nodes.index(x) - 1][x]
+	for x in prev_nodes[1:]: prev_weight += eDistance(nodes[prev_nodes.index(x) - 1], nodes[x])
 	# Insert into heapq
 	for dest_node in range(0, len(nodes)) :
-		if (weights[prev_nodes[-1]][dest_node] != -1) and (dest_node not in prev_nodes):
-			heapq.heappush(q, (prev_weight + weights[prev_nodes[-1]][dest_node] + eDistance(nodes[dest_node], nodes[end_node]), prev_nodes + (dest_node,)))
+		if (is_connected[prev_nodes[-1]][dest_node]) and (dest_node not in prev_nodes):
+			heappush(q, (prev_weight + eDistance(nodes[prev_nodes[-1]], nodes[dest_node]) + eDistance(nodes[dest_node], nodes[end_node]), prev_nodes + (dest_node,)))
 # Print
-print(heapq.heappop(q))
+print(heappop(q))
