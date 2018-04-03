650dafc950abd142ba9db1db9126c23347fea46a
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-04-04 00:09:48 +0700

    Add return on /go

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ def astar():
 	print("start \n {}".format(start))
 	print("end \n {}".format(end))
 
+	return jsonify({
+		'path': # Masukin sini wang pathnya, janlup kabari ya wujudnya kaya apa,
+		'dist': # Masukin sini juga wang distnya
+	})
+
 if __name__ == '__main__':
 	app.debug=True
 	app.run(port=5000)
