b366e9409300b8b570411e7a774b1b3fbaf21159
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-03 21:02:17 +0700

    Added safeguards

--- astar.py
+++ astar.py
@@ -1,22 +1,17 @@
-'''
-Note that with this design, 
-* Dead ends will be evicted permanently from the queue
-* If the end node can't be reached, it will infinitely loop
-'''
-
 from heapq import heappush, heappop
 from math import sqrt
 
 # The map is represented by a tuple of nodes coordinate and a boolean matrix that tells whether 2 nodes are connected 
-nodes = ((0,0), (1,0), (1,1), (1.5,1.5), (2,2)) 
+nodes = ((0,0), (1,0), (1,1), (1.5,1.5), (2,2), (3,3)) 
 is_connected = [] # Row = from
-is_connected.append([False, True, True, False, False])
-is_connected.append([True, False, False, False, False])
-is_connected.append([True, False, False, True, True])
-is_connected.append([False, False, True, False, True])
-is_connected.append([False, False, True, True, False])
+is_connected.append([False, True, True, False, False, False])
+is_connected.append([True, False, False, False, False, False])
+is_connected.append([True, False, False, True, True, False])
+is_connected.append([False, False, True, False, True, False])
+is_connected.append([False, False, True, True, False, False])
+is_connected.append([False, False, False, False, False, False])
 start_node = 0 
-end_node = 4 
+end_node = 4 # Insert 5 for "Bercanda kamu mas"
 q = [] # Prio-Q for A*. (distance, (node1, node2, ...))
 
 def eDistance(node1, node2): return sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
@@ -26,14 +21,20 @@ for dest_node in range(0, len(nodes)) :
 	if is_connected[start_node][dest_node]:
 		heappush(q, (eDistance(nodes[start_node], nodes[dest_node]) + eDistance(nodes[dest_node], nodes[end_node]), (start_node, dest_node)))
 # Start the A* algorithm
-while q[0][1][-1] != end_node: # If the shortest path yet is able to reach the end node, break
+while q: # While there are still potential path to be explored...
+	if q[0][1][-1] == end_node: break # If the shortest path yet is able to reach the end node, break
 	prev_nodes = heappop(q)[1]
 	# Compute previous distance
 	prev_weight = 0
 	for x in prev_nodes[1:]: prev_weight += eDistance(nodes[prev_nodes.index(x) - 1], nodes[x])
-	# Insert into heapq
+	# Insert the next potential path into heapq
 	for dest_node in range(0, len(nodes)) :
 		if (is_connected[prev_nodes[-1]][dest_node]) and (dest_node not in prev_nodes):
 			heappush(q, (prev_weight + eDistance(nodes[prev_nodes[-1]], nodes[dest_node]) + eDistance(nodes[dest_node], nodes[end_node]), prev_nodes + (dest_node,)))
+		# If it's a dead end, it won't be pushed again: it'll be evicted permanently from the q
+
 # Print
-print(heappop(q))
+if q:
+	print(q[0])
+else:
+	print("Bercanda kamu mas") # end_node can't be reached
